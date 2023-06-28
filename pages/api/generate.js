33a149bf4a4c2c032924329b689633296077c318
stfnylim <stfnylim@gmail.com>
2023-06-28 02:19:42 -0700

    change to recipe maker

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -15,11 +15,11 @@ export default async function (req, res) {
     return;
   }
 
-  const animal = req.body.animal || '';
-  if (animal.trim().length === 0) {
+  const ingredients = req.body.ingredients || '';
+  if (ingredients.trim().length === 0) {
     res.status(400).json({
       error: {
-        message: "Please enter a valid animal",
+        message: "Please enter valid ingredients",
       }
     });
     return;
@@ -28,9 +28,11 @@ export default async function (req, res) {
   try {
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: generatePrompt(animal),
+      prompt: generatePrompt(ingredients),
+      max_tokens: 800,
       temperature: 0.6,
     });
+    console.log(completion.data.choices);
     res.status(200).json({ result: completion.data.choices[0].text });
   } catch(error) {
     // Consider adjusting the error handling logic for your use case
@@ -48,15 +50,8 @@ export default async function (req, res) {
   }
 }
 
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+function generatePrompt(ingredients) {
+  const ingred =
+  ingredients;
+  return `Suggest a recipe for these ingredients: ${ingred}`;
 }
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
+  const [ingredientInput, setIngredientInput] = useState("");
   const [result, setResult] = useState();
 
   async function onSubmit(event) {
@@ -14,7 +14,7 @@ export default function Home() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ animal: animalInput }),
+        body: JSON.stringify({ ingredients: ingredientInput }),
       });
 
       const data = await response.json();
@@ -23,35 +23,66 @@ export default function Home() {
       }
 
       setResult(data.result);
-      setAnimalInput("");
+      setIngredientInput("");
     } catch(error) {
       // Consider implementing your own error handling logic here
       console.error(error);
       alert(error.message);
     }
+    // async function onSubmit(event) {
+    // event.preventDefault();
+    // try {
+
+    //   const response =  openAI.generateText(generatePrompt(topic), model, 800)
+    //   .then(text => {
+    //       // Logging the generated text to the console
+    //       // In the future, this will be replaced to upload the returned blog text to a WordPress site using the WordPress REST API
+    //       console.log(text);
+    //   })
+    //   .catch(error => {
+    //       console.error(error);
+    //   });
+
+    //   const data = await response.json();
+    //   if (response.status !== 200) {
+    //     throw data.error || new Error(`Request failed with status ${response.status}`);
+    //   }
+
+    //   setResult(data.result);
+    //   setIngredientInput("");
+    // } catch(error) {
+    //   // Consider implementing your own error handling logic here
+    //   console.error(error);
+    //   alert(error.message);
+    // }
   }
 
   return (
     <div>
       <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
+        <title>Mai Meal Beta</title>
+        {/* <link rel="icon" href="/dog.png" /> */}
       </Head>
 
       <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        {/* <img src="/dog.png" className={styles.icon} /> */}
+        <h3>Input ingredients you have to generate a recipe</h3>
         <form onSubmit={onSubmit}>
           <input
             type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
+            name="ingredients"
+            placeholder="i.e rice, ground beef, white onion"
+            value={ingredientInput}
+            onChange={(e) => setIngredientInput(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+          <input type="submit" value="Generate recipe" />
         </form>
-        <div className={styles.result}>{result}</div>
+        <div className={styles.result}>
+          <pre>
+          {result}
+          </pre>
+          
+          </div>
       </main>
     </div>
   );
